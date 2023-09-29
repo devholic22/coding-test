39e6da1d8723f77e5528ae90bf7977c2483544c9
devholic22 <hyunjoon.tech@gmail.com>
2023-09-29 23:52:47 +0900

    solve: 1271

---
+++ src/basic/great_rich_2.java
@@ -0,0 +1,36 @@
+package basic;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class great_rich_2 {
+
+    private static BufferedReader br;
+    private static BufferedWriter bw;
+
+    public static void main(String[] args) throws IOException {
+        init();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger n = new BigInteger(st.nextToken());
+        BigInteger m = new BigInteger(st.nextToken());
+
+        bw.write(n.divide(m) + "\n");
+        bw.write(n.mod(m) + "\n");
+        close();
+    }
+
+    private static void init() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+
+    private static void close() throws IOException {
+        bw.close();
+        br.close();
+    }
+}
