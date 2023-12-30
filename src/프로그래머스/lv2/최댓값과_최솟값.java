b57a7f6ca2922ca1f0d92a89edfd12784c171553
devholic22 <hyunjoon.tech@gmail.com>
2023-12-30 19:28:16 +0900

    docs: 문제 정보 추가

--- src/프로그래머스/lv2/최댓값과_최솟값.java
+++ src/프로그래머스/lv2/최댓값과_최솟값.java
@@ -2,6 +2,10 @@ package 프로그래머스.lv2;
 
 public class 최댓값과_최솟값 {
 
+    /*
+    SOLVED: 23.12.30 (토)
+    최댓값과 최솟값 - Lv2
+     */
     public String solution(String s) {
         String[] numbers = s.split(" ");
         int maxValue = Integer.MIN_VALUE;
