d212616d3dcb0488a752ac69be86e6f8b698e3a1
devholic22 <hyunjoon.tech@gmail.com>
2023-12-30 19:27:30 +0900

    solve: 프로그래머스 최댓값과 최솟값

---
+++ src/프로그래머스/lv2/최댓값과_최솟값.java
@@ -0,0 +1,16 @@
+package 프로그래머스.lv2;
+
+public class 최댓값과_최솟값 {
+
+    public String solution(String s) {
+        String[] numbers = s.split(" ");
+        int maxValue = Integer.MIN_VALUE;
+        int minValue = Integer.MAX_VALUE;
+        for (String number : numbers) {
+            maxValue = Math.max(Integer.parseInt(number), maxValue);
+            minValue = Math.min(Integer.parseInt(number), minValue);
+        }
+
+        return minValue + " " + maxValue;
+    }
+}
